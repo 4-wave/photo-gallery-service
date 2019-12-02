ce83c9d2b02917d38972c32d53b9dc708dcfafb3
Gurjot Singh <imasingh@ucdavis.edu>
2019-12-01 16:50:35 -0800

    removed unnecessary comments

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
- 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+
  
 app.use('/', express.static(path.join(__dirname, '../public')))
 
