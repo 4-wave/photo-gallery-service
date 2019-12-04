2976122f944753b28d5f8a3f40c5491662006285
Gurjot Singh <imasingh@ucdavis.edu>
2019-12-04 13:08:31 -0800

    refactored gallery to use states

--- client/Component/gallery.jsx
+++ client/Component/gallery.jsx
@@ -1,8 +1,3 @@
-//the page is dynamic depending on its size
-//743x706px or LESS page has just one pic
-//744x706px - 1127x706  is three pictures
-// 1128 and above is 5 pics
-
 import React from 'react';
 import styles from './styles/gallery.css'
 
@@ -10,45 +5,47 @@ class Gallery extends React.Component {
     constructor() {
         super();
         this.state = {
-            
+            img1css: styles.img1, 
+            img2css: styles.img2,
+            img3css: styles.img3,
+            img4css: styles.img4,
+            img5css: styles.img5,
+            img1url: "https://airbnbphotogallery.s3-us-west-1.amazonaws.com/bedroom6.jpg",
+            img2url: "https://airbnbphotogallery.s3-us-west-1.amazonaws.com/livingroom6.jpg",
+            img3url: "https://airbnbphotogallery.s3-us-west-1.amazonaws.com/bathroom6.jpg",
+            img4url: "https://airbnbphotogallery.s3-us-west-1.amazonaws.com/feature6.jpg",
+            img5url: "https://airbnbphotogallery.s3-us-west-1.amazonaws.com/outside6.jpg",
         };
+
+        //if only recieving three imgages in the props, then state changes to disappear
+        //how does that funtion run? it it a component did mount?
     }
 
     render() {
         //going to temporarily replace the images so i dont keep using GET reqs from AWS
         
-        const img1 = {
-            'background-image': 'url("https://airbnbphotogallery.s3-us-west-1.amazonaws.com/bedroom6.jpg")',
-        }
-        const img2 = {
-            'background-image': 'url("https://airbnbphotogallery.s3-us-west-1.amazonaws.com/bathroom6.jpg")',
-        }
-        const img3 = {
-            'background-image': 'url("https://airbnbphotogallery.s3-us-west-1.amazonaws.com/livingroom6.jpg")',
-        }
-        const img4 = {
-            'background-image': 'url("https://airbnbphotogallery.s3-us-west-1.amazonaws.com/feature6.jpg")',
-        }
-        const img5 = {
-            'background-image': 'url("https://airbnbphotogallery.s3-us-west-1.amazonaws.com/outside6.jpg")',
-        }
-        
         return (
             <div className={styles.container}>
-                <div className = {styles.img1} style = {img1}>
-                </div>
 
-                <div className={`hello ${styles.img2}`} style = {img2} >
-                </div>
+                <div className = {this.state.img1css} style = {
+                    {'background-image': `url(${this.state.img1url})`,}
+                }></div>
 
-                <div className = {styles.img3} style = {img3} >
-                </div>
+                <div className={`hello ${this.state.img2css}`} style = {
+                    {'background-image': `url(${this.state.img2url})`,}
+                } ></div>
 
-                <div className = {styles.img4} style = {img4} >
-                </div>
+                <div className = {this.state.img3css} style = {
+                    {'background-image': `url(${this.state.img3url})`,}
+                } ></div>
 
-                <div className = {styles.img5} style = {img5}>
-                </div>
+                <div className = {this.state.img4css} style = {
+                    {'background-image': `url(${this.state.img4url})`,}
+                } ></div>
+
+                <div className = {this.state.img5css} style = {
+                    {'background-image': `url(${this.state.img5url})`,}
+                } ></div>
 
             </div>
     );
--- client/app.jsx
+++ client/app.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      photos: []
     };
     // console.log(Gallery, 'HEREEE')
   }
