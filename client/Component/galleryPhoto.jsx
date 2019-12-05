2a6ee99b616018d115aa719ebc6cb6ad2de65bd6
Gurjot Singh <imasingh@ucdavis.edu>
2019-12-05 13:13:34 -0800

    added hover darken and zoom feature

--- client/Component/galleryPhoto.jsx
+++ client/Component/galleryPhoto.jsx
@@ -5,32 +5,19 @@ class GalleryPhoto extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgCss: styles[`img${props.type}`],
+      imgCss: styles[`img${props.identity}`],
       imgUrl: props.photo.url,
-      innerDivCss: styles.divInner,
     };
-  }
-
-  darkenOtherPohots(e) {
-    console.log(e.target.className.split(" ")[0]);
-    this.setState({
-      innerDivCss: styles.divInnerDarken,
-    });
-  }
-
-  undarkenOtherPhotos(e) {
-    console.log(e.target.className.split(" ")[0]); //ACTUALLY this needs to happen in the parent, since only parent can see all of the divs
-    console.log('I stopped hovering');
-    this.setState({
-      innerDivCss: styles.divInner,
-    });
   }
 
   render() {
     return (
       <div className={`hello ${this.state.imgCss}`}>
-        <div className={`${this.props.type} ${this.state.innerDivCss}`} style={{ 'background-image': `url(${this.state.imgUrl})` }} 
-        onMouseOver={this.darkenOtherPohots.bind(this)} onMouseLeave={this.undarkenOtherPhotos.bind(this)}
+        <div 
+          className={`${this.props.identity} ${this.props.css}`} 
+          style={{ 'background-image': `url(${this.state.imgUrl})` }} 
+          onMouseOver={this.props.darken} 
+          onMouseLeave={this.props.undarken}
         />
       </div>
     );
