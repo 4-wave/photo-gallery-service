fd2d23cde0e1b5a488bf9d14ee99cd7bd2b2043e
Lilly Tang <lillytang@Lillys-MacBook-Pro.local>
2020-01-08 14:13:29 -0800

    updated the get route on the client

--- client/Component/app.jsx
+++ client/Component/app.jsx
@@ -27,19 +27,21 @@ class App extends React.Component {
     } else {
       endpoint = window.location.pathname.split("/")[1];
     }
-    axios.get(`http://54.177.59.24/airbnb/listings/${endpoint}`).then(data => {
-      let urls = [];
-      data.data.map(photo => {
-        let photo_url = photo.photo_url.replace(/['"]+/g, "");
-        urls.push(photo_url);
+    axios
+      .get(`http://54.177.59.24:3008/airbnb/listings/${endpoint}`)
+      .then(data => {
+        let urls = [];
+        data.data.map(photo => {
+          let photo_url = photo.photo_url.replace(/['"]+/g, "");
+          urls.push(photo_url);
+        });
+        this.setState({
+          photos: {
+            name: "placeholder",
+            urls: urls
+          }
+        });
       });
-      this.setState({
-        photos: {
-          name: "placeholder",
-          urls: urls
-        }
-      });
-    });
   }
 
   changePage(e) {
